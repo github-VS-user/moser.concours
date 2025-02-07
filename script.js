d188af0a4fb4f2c6034361c2daa58d4e64dac601
MasterX_823 <109917976+github-VS-user@users.noreply.github.com>
2025-02-07 12:39:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ async function chargerEleves() {
 }
 
 chargerEleves();
+
+function loginEleve() {
+    window.location.href = "dashboard.html";
+}
+
+function loginAdmin() {
+    let password = prompt("Entrez le mot de passe admin :");
+    if (password === "admin123") {
+        window.location.href = "admin.html";
+    } else {
+        alert("Mot de passe incorrect !");
+    }
+}
